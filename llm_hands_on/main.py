ba579b95b4aa47db376a1557867343b280ccf87d
tosaka07 <tosakaup@gmail.com>
2024-05-22 21:27:19 +0900

    feat: fastapi

--- llm_hands_on/main.py
+++ llm_hands_on/main.py
@@ -1,2 +1,14 @@
+import uvicorn
+
+
+def main() -> None:
+    uvicorn.run(
+        "app:app",
+        host="127.0.0.1",
+        port=4598,
+    )
+
+
 if __name__ == "__main__":
-    print("Hello World!")
+    print("Starting the application")
+    main()
