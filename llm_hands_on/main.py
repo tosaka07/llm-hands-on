eede02146e7cda48d93fb58a7e6a912c231f6e5f
tosaka07 <tosakaup@gmail.com>
2024-05-22 21:52:14 +0900

    chore: remove fastapi

--- llm_hands_on/main.py
+++ llm_hands_on/main.py
@@ -1,14 +1,2 @@
-import uvicorn
-
-
-def main() -> None:
-    uvicorn.run(
-        "app:app",
-        host="127.0.0.1",
-        port=4598,
-    )
-
-
 if __name__ == "__main__":
-    print("Starting the application")
-    main()
+    print("Hello, world!")
